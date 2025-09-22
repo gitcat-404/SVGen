5caf0128bc3cbb607f46b561013f9ff6ebe9aa57
gitcat-404 <121782993+gitcat-404@users.noreply.github.com>
2025-09-22 20:21:52 +0800

    Add files via upload

---
+++ config/train_stage_RL.sh
@@ -0,0 +1,5 @@
+python RL/train-grpo.py \
+    --model_path "/path/to/your/model" \
+    --dataset_path "json_data/RL_data.json" \
+    --output_dir "training_outputs" \
+    --final_model_path "/path/to/your/saved_final_RL_model"
